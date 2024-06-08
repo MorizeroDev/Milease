c962aeb3cce8ff9d90a91e740bc7948549f0a5a4
Buger404 <ris_vb@126.com>
2024-06-08 17:10:05 +0800

    feat: color opacity utility

--- Scripts/Milease/Utils/ColorUtils.cs
+++ Scripts/Milease/Utils/ColorUtils.cs
@@ -11,5 +11,23 @@ namespace Milease.Utils
         
         public static Color RGBA(float r, float g, float b, float a)
             => new Color(r / 255f, g / 255f, b / 255f, a);
+
+        public static Color Clear(this Color color)
+        {
+            color.a = 0;
+            return color;
+        }
+        
+        public static Color Opacity(this Color color, float a)
+        {
+            color.a = a;
+            return color;
+        }
+        
+        public static Color Opacity255(this Color color, float a)
+        {
+            color.a = a / 255f;
+            return color;
+        }
     }
 }
