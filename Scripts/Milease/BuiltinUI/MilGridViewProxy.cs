431d69d06a98d1f412bc6a5747ceed1c4db46694
Buger404 <ris_vb@126.com>
2026-03-02 16:16:20 +0800

    feat: grid view support

---
+++ Scripts/Milease/BuiltinUI/MilGridViewProxy.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using Milease.Core.UI;
+using UnityEngine;
+
+namespace Milease.BuiltinUI
+{
+    public class MilGridViewProxy<T>
+    {
+        private int _columnCount;
+        private readonly MilListView _chargedListView;
+
+        private readonly List<List<T>> _listSegments = new List<List<T>>();
+        
+        /// <summary>
+        /// Create a grid view proxy class
+        /// </summary>
+        /// <param name="listView">The list view to contain sub list views</param>
+        /// <param name="columnCount">Column count</param>
+        public MilGridViewProxy(MilListView listView, int columnCount)
+        {
+            _columnCount = columnCount;
+            _chargedListView = listView;
+        }
+
+        public void SetColumnCount(int columnCount)
+        {
+            var data = new List<T>();
+            foreach (var segment in _listSegments)
+            {
+                data.AddRange(segment);
+            }
+            
+            _columnCount = columnCount;
+            
+            Set(data);
+        }
+        
+        public void Add(T item)
+        {
+            if (_listSegments.Count > 0 && _listSegments[_listSegments.Count - 1].Count < _columnCount)
+            {
+                _listSegments[_listSegments.Count - 1].Add(item);
+            }
+            else
+            {
+                _listSegments.Add(new List<T>() { item });
+                _chargedListView.Add(_listSegments[_listSegments.Count - 1]);
+            }
+        }
+
+        private void MoveForward(int row)
+        {
+            if (row >= _listSegments.Count - 1)
+            {
+                return;
+            }
+            
+            _listSegments[row].Add(_listSegments[row + 1][0]);
+
+            for (var i = row + 1; i < _listSegments.Count; i++)
+            {
+                if (i + 1 <= _listSegments.Count - 1)
+                {
+                    _listSegments[i].Add(_listSegments[i + 1][0]);
+                }
+
+                _listSegments[i].RemoveAt(0);
+            }
+        }
+        
+        public void Remove(T item)
+        {
+            for (var i = 0; i < _listSegments.Count; i++)
+            {
+                var j = _listSegments[i].IndexOf(item);
+                if (j == -1)
+                {
+                    continue;
+                }
+                
+                _listSegments[i].RemoveAt(j);
+                MoveForward(i);
+            }
+        }
+
+        public void Set(List<T> items)
+        {
+            _chargedListView.Clear();
+            _listSegments.Clear();
+            for (var i = 0; i < items.Count; i += _columnCount)
+            {
+                var segment = items.GetRange(i, Math.Min(_columnCount, items.Count - i));
+                _chargedListView.Add(segment);
+                _listSegments.Add(segment);
+            }
+        }
+    }
+}
---
+++ Scripts/Milease/BuiltinUI/MilSubGridView.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Milease.Core.Animator;
+using Milease.Core.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Milease.BuiltinUI
+{
+    [RequireComponent(typeof(MilListView))]
+    public class MilSubGridView : MilListViewItem, IBeginDragHandler, IDragHandler, IEndDragHandler, IScrollHandler
+    {
+        [Tooltip("The list view attached to this object")]
+        public MilListView SubListView;
+        [Tooltip("The parent list view, which contains these sub list views")]
+        public MilListView MixedListView;
+        
+        protected override IEnumerable<MilStateParameter> ConfigDefaultState()
+            => Array.Empty<MilStateParameter>();
+
+        protected override IEnumerable<MilStateParameter> ConfigSelectedState()
+            => Array.Empty<MilStateParameter>();
+
+        public override void OnSelect(PointerEventData eventData)
+        {
+
+        }
+
+        protected override void OnInitialize()
+        {
+
+        }
+
+        protected override void OnTerminate()
+        {
+
+        }
+
+        protected override MilInstantAnimator ConfigClickAnimation()
+            => null!;
+
+        public override void UpdateAppearance()
+        {
+            if (!(Binding is IList items))
+            {
+                return;
+            }
+            
+            SubListView.Clear();
+            
+            for (var i = 0; i < items.Count; i++)
+            {
+                if (i >= SubListView.Items.Count)
+                {
+                    SubListView.Add(items[i]);
+                }
+                else if (!Equals(SubListView.Items[i], items[i]))
+                {
+                    SubListView.UpdateItem(i, items[i]);
+                }
+            }
+        }
+
+        public override void AdjustAppearance(float pos)
+        {
+
+        }
+        
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            MixedListView.OnBeginDrag(eventData);
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            MixedListView.OnDrag(eventData);
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            MixedListView.OnEndDrag(eventData);
+        }
+
+        public void OnScroll(PointerEventData eventData)
+        {
+            MixedListView.OnScroll(eventData);
+        }
+    }
+}
